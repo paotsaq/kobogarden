29fc8541f1dc3bc791bfb14c3952f94b64110a98
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2023-10-06 17:58:58 +0100

    refactor of highlight list function

--- utils.py
+++ utils.py
@@ -40,13 +40,42 @@ def get_highlight_from_database(
     return content
 
 
+def get_list_of_highlighted_books(
+        sqlite_db_path: str
+        ):
+    def take_epub_file_name_from_path(path: str):
+        return path.split('/')[-1]
+    conn = create_connection_to_database(sqlite_db_path)
+    c = conn.cursor()
+    # Execute the SQL query
+    c.execute("""
+    SELECT 
+        unique_book_titles.BookTitle,
+        content.Attribution,
+        content.ContentID
+    FROM 
+        (SELECT DISTINCT content.title as BookTitle
+        FROM "Bookmark"
+        LEFT OUTER JOIN content
+        ON (content.contentID=Bookmark.VolumeID and content.ContentType=6)) as unique_book_titles
+    LEFT OUTER JOIN content
+    ON unique_book_titles.BookTitle = content.Title
+    """)
+    results = c.fetchall()  # Fetch all results
+    conn.close()
+    parsed = [
+            [title, author, take_epub_file_name_from_path(file)]
+            for title, author, file in results
+            ]
+    return parsed
+
+
 def expand_quote(
     quote_to_expand: str,
     context: str,
     backwards: bool,
     cons: int = 200
         ) -> str:
-
     start_index = context.find(quote_to_expand[:-1])
     if start_index == -1:
         print("Could not find context!")
@@ -69,12 +98,13 @@ def get_full_context_from_highlight(
         book_path: str,
         section_path: str
         ) -> str:
-        book = epub.read_epub(book_path)
-        if not book:
-            raise FileNotFoundError("The book doesn't seem to exist?")
-        section = None
-        while section is None:
-            section = book.get_item_with_href(section_path)
-            section_path = "/".join(section_path.split("/")[1:])
-        soup = BeautifulSoup(section.get_content(), 'html.parser').get_text()
-        return soup
+    book = epub.read_epub(book_path)
+    if not book:
+        raise FileNotFoundError("The book doesn't seem to exist?")
+    section = None
+    while section is None:
+        section = book.get_item_with_href(section_path)
+        section_path = "/".join(section_path.split("/")[1:])
+    soup = BeautifulSoup(section.get_content(), 'html.parser').get_text()
+    return soup
+
