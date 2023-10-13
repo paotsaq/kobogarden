6e9cc79c00841d9da4e4ee4e7cc99b16a610f443
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2023-10-13 20:46:51 +0100

    highlight interface is much better

--- interface.py
+++ interface.py
@@ -15,13 +15,13 @@ from utils import (
 from rich.table import Table
 from rich.text import Text
 
-class SingleHighlightWidget(Widget):
+class SingleHighlightWidget(Widget, can_focus=True):
     start = reactive(0)
     end = reactive(0)
     early_context = reactive(0)
     later_context = reactive(0)
 
-    def __init__(self, highlight: Option, soup: str, start: int, end: int) -> None:
+    def __init__(self, highlight: Option, soup: str, start: int, end: int, classes) -> None:
         super().__init__()
         self.highlight = highlight
         self.soup = soup
@@ -29,6 +29,11 @@ class SingleHighlightWidget(Widget):
         self.end = end
         self.early_context = self.start - 300
         self.later_context = self.end + 300
+        self.focus()
+        self.styles.background = "purple"
+        self.styles.width = "60%"
+        self.styles.height = "40%"
+        self.styles.padding = 2
 
     @staticmethod
     def highlight_generator(highlight: str, date: str) -> Table:
@@ -37,7 +42,7 @@ class SingleHighlightWidget(Widget):
         table.add_row(highlight)
         return table
 
-    def render(self) -> str:
+    def render(self) -> Text:
         text = Text()
         text.append(f"...{self.soup[self.early_context:self.start].strip()}", style='#828282')
         text.append(f" {self.soup[self.start:self.end].strip()} ", style='bold')
@@ -46,6 +51,8 @@ class SingleHighlightWidget(Widget):
 
 
 class SingleHighlightsScreen(Screen):
+    CSS_PATH = "option_list.tcss"
+
     def __init__(self, name: str, highlight: Option) -> None:
         super().__init__(name)
         self.highlight = highlight
@@ -54,21 +61,30 @@ class SingleHighlightsScreen(Screen):
         _, highlight, _, section, _ = get_highlight_from_database(self.highlight.id)
         soup = get_full_context_from_highlight('./burn.epub', section.split('#')[0])
         start, end = get_context_indices_for_highlight_display(soup, highlight)
-        yield SingleHighlightWidget(self.highlight, soup, start, end)
-        yield Input()
+        highlight_widget = SingleHighlightWidget(self.highlight, soup, start, end,
+                                    classes="border90").focus()
+
+        yield highlight_widget       
+        yield Input(classes='highlight_input',
+                    placeholder='further notes about the quote?')
+        yield Input(classes='highlight_input',
+                    placeholder='title for the quote tiddler?')
+        yield Input(classes='highlight_input',
+                    placeholder='space separated tags?')
         yield Header()
         yield Footer()
+        highlight_widget.focus()
 
     def on_key(self, event: events.Key) -> None:
         if event.key == "q":
             self.dismiss()
         elif event.key == "h":
             self.query_one(SingleHighlightWidget).start -= 1
-        elif event.key == "l":
+        elif event.key == "k":
             self.query_one(SingleHighlightWidget).end -= 1
         elif event.key == "j":
             self.query_one(SingleHighlightWidget).start += 1
-        elif event.key == "k":
+        elif event.key == "l":
             self.query_one(SingleHighlightWidget).end += 1
         elif event.key == "J":
             self.query_one(SingleHighlightWidget).start += 50
@@ -82,6 +98,9 @@ class SingleHighlightsScreen(Screen):
             self.query_one(SingleHighlightWidget).early_context -= 50
         elif event.key == "F":
             self.query_one(SingleHighlightWidget).later_context -= 50
+        if event.key == "q":
+            self.dismiss()
+
 
 class BookHighlightsWidget(Screen):
     def __init__(self, name: str, selected_option: Option) -> None:
@@ -110,6 +129,7 @@ class BookHighlightsWidget(Screen):
             selected_option_index = self.option_list.highlighted
             selected_option = self.option_list._options[selected_option_index]
             self.dismiss(selected_option)
+
 
 class MainScreen(App[None]):
     CSS_PATH = "option_list.tcss"
