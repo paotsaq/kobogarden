ab07668d4d99317e458798ef61a60f7b18279c4b
apinto @ lardafada <senhoralexandrepinto@protonmail.com>
2024-01-08 14:23:58 +0000

    further refactor and extend highlight feature

--- utils/highlight_handling.py
+++ utils/highlight_handling.py
@@ -1,9 +1,6 @@
 from bs4 import BeautifulSoup
 from ebooklib import epub
 import re
-from utils.const import (
-    BOOKS_DIR
-    )
 from utils.database import (
     get_highlight_from_database,
 
@@ -19,7 +16,7 @@ def get_full_context_from_highlight(
         book_path: str,
         section_path: str
         ) -> str:
-    book = epub.read_epub(BOOKS_DIR + book_path)
+    book = epub.read_epub(book_path)
     if not book:
         raise FileNotFoundError("The book doesn't seem to exist?")
     section = None
@@ -34,47 +31,67 @@ def get_full_context_from_highlight(
     return soup
 
 
+# gets the paragraph that contains a sentence.
+def get_book_sentence_that_matches_highlight(
+        h: str,
+        book_sentences: list[str]
+        ) -> int:
+    # NOTE this is still a very naive method ('x' in 'xyz')
+    start_index, _ = next(filter(lambda enum_tuple: h in enum_tuple[1],
+                                 enumerate(book_sentences)))
+    return start_index
+
+
+def break_string_into_list_of_sentences(string: str):
+    # pattern to break a string (soup or highlight) into a list of sentences,
+    # using the period ('.') as delimiter.
+    pattern = r"(?<=\.)\s*(?=\w)"
+    return re.split(pattern, string)
+
+
+def get_index_of_sentence_in_sentences_list(
+        sentence: str,
+        sentences: list[str]
+        ):
+    return next(filter(lambda enum_tuple: sentence in enum_tuple[1],
+                       enumerate(sentences)))
+
+
 # NOTE the soup is the whole context of the quote.
-# this function retrieves the paragraph containing the quote,
-# but I haven't yet accounted for quotes spanning multiple paragraphs.
-# Also, the first or last sentence of the highlight might be incomplete.
+# this function retrieves the sentence or paragraph containing the quote,
+# In the case of the first or last sentence of the highlight being incomplete,
+# the function will try to get the beginning and/or end of enclosing sentence.
+# FIXME it can handle the span of two paragraphs (many is still to be implemented)
 def get_start_and_end_of_highlight(
         soup: str,
         highlight: str
         ) -> list[str]:
 
-    def get_book_sentence_that_matches_highlight(
-            h: str,
-            book_sentences: list[str]
-            ) -> list[int, str]:
-        # gets the paragraph that contains a sentence.
-        # NOTE this is still a very naive method ('x' in 'xyz')
-        start_index, highlight_sentence_start = next(filter(lambda enum_tuple: h in enum_tuple[1],
-                                                            enumerate(book_sentences)))
-        return start_index, highlight_sentence_start
-
-    # pattern to break a string (soup or highlight) into a list of sentences,
-    # using the period ('.') as delimiter.
-    pattern = r"(?<=\.)\s*(?=\w)"
-    highlight_sentences = re.split(pattern, highlight)
+    # NOTE is this still needed?
     # # removes whitespace and newlines
     # highlight_sentences = list(filter(lambda s: s != '',
                                       # map(lambda s: s.strip(),
                                           # re.split(pattern, highlight))))
 
+    highlight_sentences = break_string_into_list_of_sentences(highlight)
     start_of_highlight = highlight_sentences[0]
 
-    # NOTE highlight ends in a single word. check tests for
+    # NOTE highlight might in a single word. check tests for
     # 'test_can_get_quote_across_two_paragraphs'
     # 'test_highlight_with_single_word_stray'
+    # FIXME there should also be a test for
+    # the same happening at the beginning
     end_of_highlight = highlight_sentences[-1]
     if len(end_of_highlight.split()) == 1:
         end_of_highlight = highlight_sentences[-2]
 
-    [start_paragraph_index, start_paragraph] = next(filter(lambda enum_tuple: start_of_highlight in enum_tuple[1],
-                                                                enumerate(soup.splitlines())))
-    [end_paragraph_index, end_paragraph] = next(filter(lambda enum_tuple: end_of_highlight in enum_tuple[1],
-                                                       enumerate(soup.splitlines())))
+    # NOTE shouldn't soup be passed to break_string_into_list_of_sentences instead?
+    [start_paragraph_index, start_paragraph] = (
+            get_index_of_sentence_in_sentences_list(start_of_highlight,
+                                                    soup.splitlines()))
+    [end_paragraph_index, end_paragraph] = (
+            get_index_of_sentence_in_sentences_list(end_of_highlight,
+                                                    soup.splitlines()))
 
     if (end_paragraph_index < start_paragraph_index):
         # something went terribly wrong; communicate the error
@@ -82,50 +99,42 @@ def get_start_and_end_of_highlight(
 
     # more than one paragraph
     if start_paragraph_index != end_paragraph_index:
-        start_book_sentences = re.split(pattern, start_paragraph)
-        start_index, _ = get_book_sentence_that_matches_highlight(start_of_highlight, start_book_sentences)
-        end_book_sentences = re.split(pattern, end_paragraph)
-        end_index, _ = get_book_sentence_that_matches_highlight(end_of_highlight, end_book_sentences)
+        start_book_sentences = break_string_into_list_of_sentences(start_paragraph)
+        start_index = get_book_sentence_that_matches_highlight(start_of_highlight,
+                                                               start_book_sentences)
+        end_book_sentences = break_string_into_list_of_sentences(end_paragraph)
+        end_index = get_book_sentence_that_matches_highlight(end_of_highlight,
+                                                             end_book_sentences)
         # concatenate the start_of_highlight paragraph,
         # any paragraphs in between,
         # and then the end_of_highlight paragraph
-        return ("\n\n".join([" ".join(start_book_sentences[start_index:]),
-                             # middle_book_paragraphs,
-                             " ".join(end_book_sentences[:end_index + 1])]))
+        return ([" ".join(start_book_sentences[start_index:]),
+                 "\n\n",
+                 # middle_book_paragraphs,
+                 " ".join(end_book_sentences[:end_index + 1])])
 
     else:
-        book_sentences = re.split(pattern, start_paragraph)
-        start_index, _ = get_book_sentence_that_matches_highlight(start_of_highlight, book_sentences)
-        end_index, _ = get_book_sentence_that_matches_highlight(end_of_highlight, book_sentences)
-        return (" ".join(book_sentences[start_index:end_index + 1]))
+        book_sentences = break_string_into_list_of_sentences(start_paragraph)
+        start_index = get_book_sentence_that_matches_highlight(start_of_highlight,
+                                                               book_sentences)
+        end_index = get_book_sentence_that_matches_highlight(end_of_highlight,
+                                                             book_sentences)
+        return (book_sentences[start_index:end_index + 1])
 
 
-def expand_quote(
-    quote_to_expand: str,
-    context: str,
+def expand_found_highlight(
+    highlight_to_expand: list[str],
+    soup: list[str],
+    amount_of_sentences: int,
     backwards: bool,
-    cons: int = 200
-        ) -> str:
-    quote_to_expand = quote_to_expand
-    start_index = context.find(quote_to_expand[:-1])
-    if start_index == -1:
-        # NOTE THIS IS JUST A QUICKFIX
-        start_index = context.find(quote_to_expand[:70])
-        if start_index == -1:
-            print("Could not find context!")
-            return ""
-    end_index = start_index + len(quote_to_expand)
-    if backwards:
-        new_index = context[start_index - cons:start_index].rfind('.')
-        if new_index == -1:
-            return expand_quote(quote_to_expand, context, backwards, cons + 200)
-        new_quote = context[start_index - cons + new_index + 1:end_index].strip()
-    else:
-        new_index = context[start_index:end_index + cons].rfind('.')
-        if new_index == -1:
-            return expand_quote(quote_to_expand, context, backwards, cons + 200)
-        new_quote = context[start_index:start_index + new_index + 1].strip()
-    return new_quote
+        ) -> list[str]:
+    # decide whether to look for first or last sentence of highlight
+    anchor = -1 if backwards else 0
+    broken_soup = break_string_into_list_of_sentences(soup)
+    highlight_location = broken_soup.index(highlight_to_expand[anchor])
+    return ([broken_soup[highlight_location - amount_of_sentences]] + highlight_to_expand
+            if backwards
+            else highlight_to_expand + [broken_soup[highlight_location + amount_of_sentences]])
 
 
 def get_context_indices_for_highlight_display(
