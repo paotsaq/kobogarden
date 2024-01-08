9bb08a1652de47080178f750eec7e991ed98acc2
apinto @ lardafada <senhoralexandrepinto@protonmail.com>
2024-01-08 14:23:58 +0000

    further refactor and extend highlight feature

--- tests.py
+++ tests.py
@@ -12,9 +12,9 @@ from utils.tiddler_handling import (
     produce_highlight_tiddler_string
 )
 from utils.highlight_handling import (
-        expand_quote,
         get_full_context_from_highlight,
         get_start_and_end_of_highlight,
+        expand_found_highlight
         )
 from utils.const import (
     SQLITE_DB_PATH,
@@ -25,8 +25,10 @@ TEST_EPUB_JANEEYRE = 'jane-eyre.epub'
 TEST_EPUB_BERLIN = 'berlin.epub'
 TEST_EPUB_BURN = 'burn.epub'
 TEST_EPUB_HOWTO = 'howto.epub'
+TEST_EPUB_SCATTERED = 'scattered_minds.epub'
 DESIRED_SENTENCE = """It is in vain to say human beings ought to be satisfied with tranquillity: they must have action, and they will make it if they cannot find it."""
 ANOTHER_DESIRED_SENTENCE = """if she were a nice, pretty child, one might compassionate her forlornness"""
+TEST_BOOKS_DIR = 'test_books/'
 
 
 
@@ -86,10 +88,10 @@ class TestingFindingQuoteInEpubFiles(unittest.TestCase):
         PARTIAL_QUOTE = 'relationship between the gate and the all-important circulation of traffic sparked another'
         FULL_QUOTE = 'The relationship between the gate and the all-important circulation of traffic sparked another debate.'
         title, highlight, _, section, _ = get_highlight_from_database("1fb6bc3a-7d4f-40a0-ab62-c095fa62b26a")
-        soup = get_full_context_from_highlight(TEST_EPUB_BERLIN, 'text/part0011.html')
+        soup = get_full_context_from_highlight(TEST_BOOKS_DIR + TEST_EPUB_BERLIN, 'text/part0011.html')
         self.assertIsNotNone(soup)
-        self.assertEqual(get_start_and_end_of_highlight(soup, PARTIAL_QUOTE),
-                         FULL_QUOTE)
+        result = " ".join(get_start_and_end_of_highlight(soup, PARTIAL_QUOTE))
+        self.assertEqual(result, FULL_QUOTE)
 
     # the highlight is well-delimited (no partial sentences before or after).
     def test_can_find_simple_full_quote_in_epub_file(self):
@@ -97,10 +99,10 @@ class TestingFindingQuoteInEpubFiles(unittest.TestCase):
         FULL_QUOTE = 'The relationship between the gate and the all-important circulation of traffic sparked another debate.'
         self.assertEqual(QUOTE, FULL_QUOTE)
         title, highlight, _, section, _ = get_highlight_from_database("1fb6bc3a-7d4f-40a0-ab62-c095fa62b26a")
-        soup = get_full_context_from_highlight(TEST_EPUB_BERLIN, 'text/part0011.html')
+        soup = get_full_context_from_highlight(TEST_BOOKS_DIR + TEST_EPUB_BERLIN, 'text/part0011.html')
         self.assertIsNotNone(soup)
-        self.assertEqual(get_start_and_end_of_highlight(soup, QUOTE),
-                         FULL_QUOTE)
+        result = " ".join(get_start_and_end_of_highlight(soup, QUOTE))
+        self.assertEqual(result, FULL_QUOTE)
 
     # the highlight is partial at the beginning.
     # in this case, the first sentence is presented in its complete form.
@@ -109,10 +111,10 @@ class TestingFindingQuoteInEpubFiles(unittest.TestCase):
         FULL_QUOTE = """Our modern food production system violates the fundamental laws of ecology. When we include the fossil fuel energy consumed in food production, we burn 8 calories for every calorie of food we produce. That’s not a great recipe for avoiding extinction."""
         title, highlight, _, section, _ = get_highlight_from_database("1fb6bc3a-7d4f-40a0-ab62-c095fa62b26a")
         # NOTE: the OEBPS/ must be ommited - not sure why!
-        soup = get_full_context_from_highlight(TEST_EPUB_BURN, section.split('#')[0])
+        soup = get_full_context_from_highlight(TEST_BOOKS_DIR + TEST_EPUB_BURN, section.split('#')[0])
         self.assertIsNotNone(soup)
-        self.assertEqual(get_start_and_end_of_highlight(soup, PARTIAL_QUOTE),
-                         FULL_QUOTE)
+        result = " ".join(get_start_and_end_of_highlight(soup, PARTIAL_QUOTE))
+        self.assertEqual(result, FULL_QUOTE)
 
     # the highlight is partial at the end.
     # in this case, the last sentence is presented in its complete form.
@@ -120,9 +122,9 @@ class TestingFindingQuoteInEpubFiles(unittest.TestCase):
         PARTIAL_QUOTE = """When the molecules in a pound of nitroglycerin (chemical formula: 4C3H5N3O9) are broken into nitrogen (N2), water (H2O), carbon monoxide (CO), and oxygen (O2) during detonation, it violently releases enough energy (730 kilocalories)"""
         FULL_QUOTE = """When the molecules in a pound of nitroglycerin (chemical formula: 4C3H5N3O9) are broken into nitrogen (N2), water (H2O), carbon monoxide (CO), and oxygen (O2) during detonation, it violently releases enough energy (730 kilocalories) to launch a 165-pound man two and a half miles straight up into the sky (which would be work) or vaporize him (which would be heat), or some combination of the two."""
         title, highlight, _, section, _ = get_highlight_from_database("c71f3857-162f-43c2-b783-d63eb63b6957")
-        soup = get_full_context_from_highlight(TEST_EPUB_BURN, section.split('#')[0])
-        self.assertEqual(get_start_and_end_of_highlight(soup, PARTIAL_QUOTE),
-                         FULL_QUOTE)
+        soup = get_full_context_from_highlight(TEST_BOOKS_DIR + TEST_EPUB_BURN, section.split('#')[0])
+        result = " ".join(get_start_and_end_of_highlight(soup, PARTIAL_QUOTE))
+        self.assertEqual(result, FULL_QUOTE)
 
     # the highlight is partial at the beginning and end.
     # in this case, both sentences are presented in its complete form.
@@ -130,9 +132,9 @@ class TestingFindingQuoteInEpubFiles(unittest.TestCase):
         PARTIAL_QUOTE = """pound of nitroglycerin (chemical formula: 4C3H5N3O9) are broken into nitrogen (N2), water (H2O), carbon monoxide (CO), and oxygen (O2) during detonation, it violently releases enough energy (730 kilocalories)"""
         FULL_QUOTE = """When the molecules in a pound of nitroglycerin (chemical formula: 4C3H5N3O9) are broken into nitrogen (N2), water (H2O), carbon monoxide (CO), and oxygen (O2) during detonation, it violently releases enough energy (730 kilocalories) to launch a 165-pound man two and a half miles straight up into the sky (which would be work) or vaporize him (which would be heat), or some combination of the two."""
         title, highlight, _, section, _ = get_highlight_from_database("c71f3857-162f-43c2-b783-d63eb63b6957")
-        soup = get_full_context_from_highlight(TEST_EPUB_BURN, section.split('#')[0])
-        self.assertEqual(get_start_and_end_of_highlight(soup, PARTIAL_QUOTE),
-                         FULL_QUOTE)
+        soup = get_full_context_from_highlight(TEST_BOOKS_DIR + TEST_EPUB_BURN, section.split('#')[0])
+        result = " ".join(get_start_and_end_of_highlight(soup, PARTIAL_QUOTE))
+        self.assertEqual(result, FULL_QUOTE)
 
     # the highlight not only is malformed, it also spans line-breaks.
     # the '4' at the end of a sentence is a bummer,
@@ -144,7 +146,7 @@ class TestingFindingQuoteInEpubFiles(unittest.TestCase):
 
 In the face of the increasingly materialist and pragmatic orientation of our age…it would not be eccentric in the future to contemplate a society in which those who live for the pleasures of the mind will no longer have the right to demand their place in the sun. The writer, the thinker, the dreamer, the poet, the metaphysician, the observer…he who tries to solve a riddle or to pass judgement will become an anachronistic figure, destined to disappear from the face of the earth like the ichthyosaur and the mammoth."""
         title, highlight, _, section, book_path = get_highlight_from_database("b22af57c-2b8c-494f-ac99-96fa698f1dac")
-        soup = get_full_context_from_highlight(TEST_EPUB_HOWTO, section.split('#')[0])
+        soup = get_full_context_from_highlight(TEST_BOOKS_DIR + TEST_EPUB_HOWTO, section.split('#')[0])
         paragraphs = get_start_and_end_of_highlight(soup, highlight)
         text = reduce(lambda x, y: x + y,
                       paragraphs)
@@ -155,12 +157,15 @@ In the face of the increasingly materialist and pragmatic orientation of our age
 
     def test_can_extend_quote_forwards_until_next_period(self):
         PARTIAL_QUOTE = """When the molecules in a pound of nitroglycerin (chemical formula: 4C3H5N3O9) are broken into nitrogen (N2), water (H2O), carbon monoxide (CO), and oxygen (O2) during detonation, it violently releases enough energy (730 kilocalories)"""
-        FULL_QUOTE = """When the molecules in a pound of nitroglycerin (chemical formula: 4C3H5N3O9) are broken into nitrogen (N2), water (H2O), carbon monoxide (CO), and oxygen (O2) during detonation, it violently releases enough energy (730 kilocalories) to launch a 165-pound man two and a half miles straight up into the sky (which would be work) or vaporize him (which would be heat), or some combination of the two. This brings us to our last point about energy: it can be converted among its many forms—kinetic energy, heat, work, chemical energy, and so on—but it can never be lost."""
+        FIRST_FOUND_QUOTE = """When the molecules in a pound of nitroglycerin (chemical formula: 4C3H5N3O9) are broken into nitrogen (N2), water (H2O), carbon monoxide (CO), and oxygen (O2) during detonation, it violently releases enough energy (730 kilocalories) to launch a 165-pound man two and a half miles straight up into the sky (which would be work) or vaporize him (which would be heat), or some combination of the two."""
+        FULL_QUOTE = " ".join([FIRST_FOUND_QUOTE,
+                               """This brings us to our last point about energy: it can be converted among its many forms—kinetic energy, heat, work, chemical energy, and so on—but it can never be lost."""])
         title, highlight, _, section, _ = get_highlight_from_database("c71f3857-162f-43c2-b783-d63eb63b6957")
-        soup = get_full_context_from_highlight(TEST_EPUB_BURN, section.split('#')[0])
-        res = expand_quote(PARTIAL_QUOTE, soup, False)
-        res = expand_quote(res, soup, False)
-        self.assertEqual(res, FULL_QUOTE)
+        soup = get_full_context_from_highlight(TEST_BOOKS_DIR + TEST_EPUB_BURN, section.split('#')[0])
+        found_highlight = get_start_and_end_of_highlight(soup, PARTIAL_QUOTE)
+        self.assertEqual(" ".join(found_highlight), FIRST_FOUND_QUOTE)
+        expanded_highlight = expand_found_highlight(found_highlight, soup, 1, False)
+        self.assertEqual(" ".join(expanded_highlight), FULL_QUOTE)
 
     # highlight is well-formed, but ends with a single stray word.
     # eg: "Well-formed highlight. But then. Oops"
@@ -171,12 +176,10 @@ In the face of the increasingly materialist and pragmatic orientation of our age
         HIGHLIGHT_ID = "302e8612-9b65-43b0-b871-e9943038afc6"
         FULL_QUOTE = "The shock of self-recognition many adults experience on learning about ADD is both exhilarating and painful. It gives coherence, for the first time, to humiliations and failures, to plans unfulfilled and promises unkept, to gusts of manic enthusiasm that consume themselves in their own mad dance, leaving emotional debris in their wake, to the seemingly limitless disorganization of activities, of brain, car, desk, room."
         title, highlight, _, section, book_path = get_highlight_from_database(HIGHLIGHT_ID)
-        soup = get_full_context_from_highlight("scattered_minds.epub", section.split('#')[0])
+        soup = get_full_context_from_highlight(TEST_BOOKS_DIR + TEST_EPUB_SCATTERED, section.split('#')[0])
         self.assertNotEqual(soup, None)
-        paragraphs = get_start_and_end_of_highlight(soup, highlight)
-        text = reduce(lambda x, y: x + y,
-                      paragraphs)
-        self.assertEqual(text, FULL_QUOTE)
+        result = " ".join(get_start_and_end_of_highlight(soup, highlight))
+        self.assertEqual(result, FULL_QUOTE)
 
 
 class TestingCreationOfHighlightTiddler(unittest.TestCase):
