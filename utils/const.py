7d6a50798ccbc75576e0bdad59ec628b45a1c519
apinto @ lardafada <senhoralexandrepinto@protonmail.com>
2024-08-09 09:43:16 +0100

    updated db path; this should be env var anyway

--- utils/const.py
+++ utils/const.py
@@ -5,7 +5,7 @@ BOOKS_DIR = BASE_DIR + "books/"
 TIDDLERS_PATH = BASE_DIR + 'paogarden/tiddlers/'
 PROJECT_DIR = BASE_DIR + "kobo_highlights/"
 SQLITE_DB_PATH = PROJECT_DIR
-SQLITE_DB_NAME = "test_kobo_db.sqlite"
+SQLITE_DB_NAME = "my_kobo_db.sqlite"
 EXISTING_IDS_FILE = "kobo highlight ids of quotes.tid"
 DATABASE_PATH = SQLITE_DB_PATH + SQLITE_DB_NAME
 
